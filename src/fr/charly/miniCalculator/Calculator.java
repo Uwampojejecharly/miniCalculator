f95a2a6efee8bffba3db662b22748248c21fa8ab
unknown <uwampojejecharly@gmail.com>
2024-01-16 16:41:05 +0200

    Added listeners in the mini calculator

--- src/fr/charly/miniCalculator/Calculator.java
+++ src/fr/charly/miniCalculator/Calculator.java
@@ -3,6 +3,8 @@ package fr.charly.miniCalculator;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,31 +15,47 @@ import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 public class Calculator extends JFrame {
 
+	private static final long serialVersionUID = 6646703771457169932L;
 	private JPanel panel;
-	private JTextField textField;
+	private JTextField display;
+	private String numberString = "";
 
 	public Calculator() {
 		super("Calculator");
 
 		setLayout(new BorderLayout());
 
-		textField = new JTextField();
-		textField.setPreferredSize(new Dimension(400, 100));
-		add(textField, BorderLayout.NORTH);
+		display = new JTextField();
+		add(display, BorderLayout.NORTH);
 
 		panel = new JPanel(new GridLayout(4, 4));
 
-		String[] labels = { "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "", "0", "=", "/" };
+		String[] labels = { "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "", "0", "/", "=" };
 		for (String label : labels) {
 			JButton button = new JButton(label);
+			button.addActionListener(new ButtonClickListener());
 			panel.add(button);
 		}
 
-		this.add(panel);
+		this.add(panel, BorderLayout.CENTER);
+		display = new JTextField();
+		display.setPreferredSize(new Dimension(400, 100));
+		display.setHorizontalAlignment(JTextField.RIGHT);
+		this.add(display, BorderLayout.NORTH);
 		this.setTitle("Calculator");
 		this.setSize(400, 500);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private class ButtonClickListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			String command = ((JButton) e.getSource()).getText();
+			if (command.matches("[0-9/*+-]")) {
+				numberString += command;
+				display.setText(numberString);
+			}
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
