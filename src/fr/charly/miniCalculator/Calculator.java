25364ccee3f60c8e37faf36cb6dd0386865451b1
unknown <uwampojejecharly@gmail.com>
2024-01-24 11:57:35 +0200

    Added the functions that enable the calculations

--- src/fr/charly/miniCalculator/Calculator.java
+++ src/fr/charly/miniCalculator/Calculator.java
@@ -3,8 +3,6 @@ package fr.charly.miniCalculator;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,12 +11,14 @@ import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
+/**
+ * This class represents a simple calculator GUI application.
+ */
 public class Calculator extends JFrame {
 
 	private static final long serialVersionUID = 6646703771457169932L;
 	private JPanel panel;
 	private JTextField display;
-	private String numberString = "";
 
 	public Calculator() {
 		super("Calculator");
@@ -31,31 +31,20 @@ public class Calculator extends JFrame {
 		panel = new JPanel(new GridLayout(4, 4));
 
 		String[] labels = { "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "", "0", "/", "=" };
+		ButtonClickListener buttonClickListener = new ButtonClickListener(display);
 		for (String label : labels) {
-			JButton button = new JButton(label);
-			button.addActionListener(new ButtonClickListener());
+			JButton button = new JButton( label);
+			button.addActionListener(buttonClickListener);
 			panel.add(button);
 		}
 
-		this.add(panel, BorderLayout.CENTER);
-		display = new JTextField();
+		add(panel, BorderLayout.CENTER); 
 		display.setPreferredSize(new Dimension(400, 100));
 		display.setHorizontalAlignment(JTextField.RIGHT);
-		this.add(display, BorderLayout.NORTH);
-		this.setTitle("Calculator");
-		this.setSize(400, 500);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
-	private class ButtonClickListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			String command = ((JButton) e.getSource()).getText();
-			if (command.matches("[0-9/*+-]")) {
-				numberString += command;
-				display.setText(numberString);
-			}
-		}
+		setTitle("Calculator");
+		setSize(400, 500);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public static void main(String[] args) throws Exception {
